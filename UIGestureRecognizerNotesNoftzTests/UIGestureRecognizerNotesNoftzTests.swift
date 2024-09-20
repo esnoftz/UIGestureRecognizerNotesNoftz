c43d55d2ca0e3152d81aa27d9b8e14cb0b8da5e9
Evangeline Noftz <esnoftz@gmail.com>
2024-09-20 07:57:04 -0500

    Initial Commit

---
+++ UIGestureRecognizerNotesNoftzTests/UIGestureRecognizerNotesNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIGestureRecognizerNotesNoftzTests.swift
+//  UIGestureRecognizerNotesNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 9/20/24.
+//
+
+import Testing
+@testable import UIGestureRecognizerNotesNoftz
+
+struct UIGestureRecognizerNotesNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
